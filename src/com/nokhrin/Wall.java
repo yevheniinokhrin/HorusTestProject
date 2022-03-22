0d82f66f159967cf0b2e04fbbbf7e7fd92febda9
yevheniinokhrin <233315@edu.p.lodz.pl>
2022-03-22 13:11:08 +0200

    implemented methods

--- src/com/nokhrin/Wall.java
+++ src/com/nokhrin/Wall.java
@@ -2,23 +2,28 @@ package com.nokhrin;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Wall implements Structure {
-    private List blocks;
+    private List<Block> blocks;
+    private CompositeBlock compositeBlock = null;
+    {
+        blocks = compositeBlock.getBlocks();
+    }
 
     @Override
     public Optional findBlockByColor(String color) {
-        return Optional.empty();
+        return blocks.stream().filter(block -> block.getColor().equals(color)).findAny();
     }
 
     @Override
     public List findBlocksByMaterial(String material) {
-        return null;
+        return blocks.stream().filter(block -> block.getMaterial().equals(material)).collect(Collectors.toList());
     }
 
     @Override
     public int count() {
-        return 0;
+        return blocks.size();
     }
 }
 
